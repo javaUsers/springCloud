bdc3b31c47cd47c6ba48634f1331b2eaf7e3848d
xiaomo <xiaomo@xiaomo.info>
2018-06-23 14:37:40 +0800

    id自增

--- product/src/main/java/info/xiaomo/product/entity/Product.java
+++ product/src/main/java/info/xiaomo/product/entity/Product.java
@@ -3,6 +3,8 @@ package info.xiaomo.product.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Date;
@@ -27,6 +29,7 @@ import java.util.Date;
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int productId;
 
     private String productName;
