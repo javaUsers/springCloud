c21c9dfadbaf7d9a4e2e4534cc886a8edc582f22
胡朋 <hupeng@duiba.com.cn>
2018-07-04 19:24:20 +0800

    添加消费者

--- eurekaConsumer/src/main/java/info/xiaomo/eurekaconsumer/EurekaConsumerApplication.java
+++ eurekaConsumer/src/main/java/info/xiaomo/eurekaconsumer/EurekaConsumerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+@EnableTurbine
 public class EurekaConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaConsumerApplication.class, args);
